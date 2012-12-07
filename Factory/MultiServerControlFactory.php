4d38cbe2d46b0a4d76183a3f4dc0744d5b6ff8d7
F481 <fabi.kcirf@gmail.com>
2012-12-07 22:50:43 +0100

    added basic classes

---
+++ Factory/MultiServerControlFactory.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: fabian
+ * Date: 12/7/12
+ * Time: 10:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class MultiServerControlFactory
+{
+
+}
