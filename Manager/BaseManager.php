9310aea8790a63beef9a7a0ffbf2064cd8760d67
F481 <fabi.kcirf@gmail.com>
2012-12-07 23:01:56 +0100

    added basic functions

--- Manager/BaseManager.php
+++ Manager/BaseManager.php
@@ -6,7 +6,11 @@
  * Time: 10:47 PM
  * To change this template use File | Settings | File Templates.
  */
-class BaseManager
+abstract class BaseManager
 {
+    abstract public function start();
 
+    abstract public function stop();
+
+    abstract public function restart();
 }
