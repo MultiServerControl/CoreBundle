fb535675ce730cd5122ee90e02dd745bb7544ddc
F481 <fabi.kcirf@gmail.com>
2012-12-08 00:20:56 +0100

    updated comment template

--- Manager/BaseManager.php
+++ Manager/BaseManager.php
@@ -2,11 +2,12 @@
 namespace MultiServerControl\CoreBundle\Manager;
 
 /**
- * Created by JetBrains PhpStorm.
- * User: fabian
- * Date: 12/7/12
- * Time: 10:47 PM
- * To change this template use File | Settings | File Templates.
+ * File: BaseManager.php
+ * Date: 12/8/12
+ *
+ * (c) Fabian Frick <fabi.kcirf@gmail.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 abstract class BaseManager
 {
